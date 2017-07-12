24e061f6b58e5c15b21e8079245e0d4fcc109f98
CEFI-Kenny <bo.zhao@cefi.ca>
2017-07-12 16:10:38 -0400

    Add webbrowser and sys.argv in python

--- python.py
+++ python.py
@@ -1,6 +1,7 @@
+
 # teriminate a program
 import sys
-sys.exit()
+# sys.exit()
 
 
 
@@ -8,11 +9,26 @@ sys.exit()
 import pyperclip
 
 # copy to clip board
-pyperclip.copy("some text here")
+# pyperclip.copy("some text here")
 # paste from clip board
 content_in_clip_board = pyperclip.paste()
 
 
 
-# import shell utility module
-import shutil
+import os, webbrowser
+
+# get command line arguments
+# e.g. python3 python.py arg1 arg2
+#      sys.argv = ["python.py", "arg1", "arg2"]
+
+# check if there is at least one command line argument
+if len(sys.argv) > 1:
+    # form the address string without name of the program
+    address = " ".join(sys.argv[1:])
+else:
+    # get address from clipboard
+    address = pyperclip.paste()
+
+# e.g. python3 python.py 3221 North Service Road, Burlington, ON.
+# webbrowser.open("https://www.google.com/maps/place/" + address)
+
