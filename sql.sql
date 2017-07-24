e1ada6e74974a39a764bac117c111ebea9056d91
Bo Zhao <bo.zhao@cefi.ca>
2017-07-24 15:09:46 -0400

    Add sql.sql

---
+++ sql.sql
@@ -0,0 +1,9 @@
+
+-- alter is used to add, delete or modify columns in an existing table
+-- is also to add and drop various constraints on an existing table
+ALTER TABLE table_name
+ADD column_name datatype;
+
+ALTER TABLE table_name
+DROP COLUMN column_name;
+
